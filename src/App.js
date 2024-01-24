49cb0500d9dc8abbd2b925f7464d862487cea1d3
keval.mathiya <kevalmathiya123@gmail.com>
2024-01-24 09:03:34 +0530

    add files

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+import data from "./data";
+import Tours from "./Components/Tours";
+import { useState } from "react";
+
+function App() {
+  const [tours, setTours] = useState(data);
+
+  function removeTour(id) {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  }
+
+  if (tours.length === 0) {
+    return (
+      <div className="refresh">
+        <h2>No Tours Left</h2>
+        <button className="btn-white" onClick={() => setTours(data)}>
+          Refresh
+        </button>
+      </div>
+    );
+  }
+  return (
+    <div className="app">
+      <Tours tours={tours} removeTour={removeTour}></Tours>
+    </div>
+  );
+}
+
+export default App;
